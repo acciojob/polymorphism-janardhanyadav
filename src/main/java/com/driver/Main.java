01e0b46535ddfe2e87248595fe8b8027fa0627ef
Janardhan Biharilal Yadav <63313078+janardhanyadav@users.noreply.github.com>
2023-08-13 19:58:33 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
-public class Main {
 
+
+public class Main {
+  public static class Product{
+    public int product(int x, int y) {
+      return x*y;
+    }
+    
+    // @Overload
+    public  int product(int x , int y , int z){
+      return (x*y*z);
+    }
+
+    // @Overload
+    
+    public  double product(double x, double y) {
+      return x*y;
+    } 
+  }
+
+  public static void main(String[] args){
+    Product p = new Product();
+    p.product(10,20);
+
+    p.product(10, 20 , 30 );
+
+    p.product(10.1,20.2);
+  }
 }
